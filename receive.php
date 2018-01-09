a1a45228624e3dd00bf31d4ac2b54a5b51fc2be7
petershyu <35246301+petershyu@users.noreply.github.com>
2018-01-09 11:45:33 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,5 +1,5 @@
 <?php
-$json_str = file_get contents('php://input'); //接收request 的 body
+$json_str = file_get_contents('php://input'); //接收request 的 body
 $json_obj = json_decode($json_str); // 轉json格式
 
 $myfile = fopen("log.txt","w+") or die("unable to open file!"); // 設定一個log.txt檔來印訊息
