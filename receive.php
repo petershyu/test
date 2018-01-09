4ecd9e4d6317415d0cb01b4bb631b38267ad3974
petershyu <35246301+petershyu@users.noreply.github.com>
2018-01-09 14:51:54 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -26,7 +26,7 @@
 				"messages" => array (
 					array (
 						"type" => "text",
-						"text" => "Hello!!!, YOU SAY ".$sender_txt
+						"text" => "Hello, YOU SAY ".$sender_txt
 					)
 				)
 			);
@@ -40,6 +40,34 @@
 						"type" => "image",
 						"originalContentUrl" => "https://www.w3schools.com/css/paris.jpg",
 						"previewImageUrl" => "https://www.nasa.gov/sites/default/themes/NASAPortal/images/feed.png"
+					)
+				)
+			);
+        		break;
+		 case "location":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "location",
+						"title" => "my location",
+						"address" => "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+            					"latitude" => 35.65910807942215,
+						"longitude" => 139.70372892916203
+					)
+				)
+			);
+        		break;
+		case "sticker":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+        		$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "sticker",
+						"packageId" => "1",
+						"stickerId" => "1"
 					)
 				)
 			);
